f2f03ed44eeaa9a36cd5e4263e3787d8ee04126f
Assassin#1234 <chordsandfrets@gmail.com>
2021-12-03 21:14:02 +0530

    initial commit

---
+++ functions/getImageURL.js
@@ -0,0 +1,33 @@
+const random = require('random-words')(1);
+const fs = require('fs');
+const request = require('request');
+const gis = require('g-i-s');
+const jimp = require('jimp');
+module.exports = {
+	async something() {
+		await gis(random + 'wallpaper', logResults);
+	},
+};
+async function logResults(error, results) {
+	if (error) {
+		console.log(error);
+	}
+	else {
+		const uri = await results[Math.floor(Math.random() * results.length)];
+		request.head(uri, function(err, res) {
+			console.log('content-type:', res.headers['content-type']);
+			console.log('content-length:', res.headers['content-length']);
+
+			setTimeout(() => {
+				request(uri).pipe(fs.createWriteStream('nft.png'));
+			}, 5000);
+
+			jimp.read('nft.png', (err, nft) => {
+				if(err) return console.log(err);
+				nft
+					.pixelate(85)
+					.write('nft2.png');
+			});
+		});
+	}
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const functions = require('./functions/getImageURL');
+
+async function Init() {
+	await functions.something();
+}
+
+Init();
